8ae6c620fa6752acbfaa7f1c5ca923ab9cffaec1
Soham Dutta <123827172+123456788940@users.noreply.github.com>
2023-12-14 14:11:53 +0530

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -3,9 +3,12 @@
 pragma solidity ^0.8;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract ERC20Token is ERC20 {
-    constructor() ERC20("coin token", "ct") {
-        _mint(msg.sender, 10000000000000000 * 10 * 18);
-    }
+contract ERC20contract is ERC20 {
+    address public realEstateOwner;
+    string propertyDetails;
+constructor(string memory _name, string memory symbol, address owner, string memory details) ERC20(_name, symbol)  {
+realEstateOwner = owner;
+propertyDetails = details;
+_mint(owner, 100000000000000000*10**18);
+}
 }
