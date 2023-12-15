970cd4d8a8fb6621ce2d3a7a863bf45d215addc5
Soham Dutta <123827172+123456788940@users.noreply.github.com>
2023-12-15 12:12:43 +0530

    Update iquidityPool.sol

--- iquidityPool.sol
+++ iquidityPool.sol
@@ -1,93 +1,92 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
-
 import "./ERC20.sol";
 
-
-// Import UniswapV2 interfaces
-import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
-import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
-import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
-
-
-contract ERC20Contract is ERC20 {
-     address public owner;
-   modifier onlyOwner {
+contract defineGovToken {
+      address public owner;
+modifier onlyOwner() {
+  
     require(msg.sender == owner, "only owner has access");
     _;
-   }
+}
 
-   IUniswapV2Router02 public uniswapRouter;
-   address public uniswapPair;
+
+    ERC20 public governanceToken;
+    struct Voting {
+    uint  votingId;
+    bool votingPowerGiven;
+    uint voteValue;
+    bool voted;
+    }
+     uint public members;
+     uint public votingPowerCounter;
+
+    struct addAsMember{
+       uint contribution;
+       uint tenureWanted;
+       bool memberAdded;
+        }
+
+    mapping(address => addAsMember) public Member;
+    mapping(uint => Voting) public VotingPoweer;
+
+    constructor(address tokenAddress) {
+        governanceToken = ERC20(tokenAddress);
+        votingPowerCounter = 0;
+        owner = msg.sender;
+    }
+
+    function AddMember(uint _contribution, uint _tenureWanted) public onlyOwner{
+        require(!Member[msg.sender].memberAdded, "Already added");
+        require(_contribution >= 100, "amount must be equal or greater than 100");
+
+        Member[msg.sender] = addAsMember ({
+
+        
+     contribution: _contribution,
+     tenureWanted: _tenureWanted,
+   
+     memberAdded: true
+   
+
+    });
+    members++;
+
+
+    governanceToken.transferFrom(msg.sender, address(this), _contribution);
+ 
+
     
-
-   constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
-    owner = msg.sender;
-     IUniswapV2Router02 _uniswapRouter =   IUniswapV2Router02(
-                      0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D
-     );
-         uniswapRouter = _uniswapRouter;
-
-         uniswapPair =  IUniswapV2Factory(_uniswapRouter.factory()).createPair(
-            address(this),
-            _uniswapRouter.WETH()
-
-         );
-   }
-
-   function addLiquidity(uint amountToken) external onlyOwner {
-    _approve(msg.sender, address(uniswapRouter), amountToken);
-    uint balance = balanceOf(address(this));
-    _transfer(msg.sender, address(this), amountToken);
-    uniswapRouter.addLiquidityETH{value: address(this).balance}(
-        address(this),
-        balance,
-        0,
-        0,
-        owner,
-        block.timestamp + 3600
+    }
 
 
+    function votingPoweer(uint _votingId, uint _voteValue) public onlyOwner {
+            require(Member[msg.sender].memberAdded, "Already added");
+        require(!VotingPoweer[votingPowerCounter].votingPowerGiven, "already voted");
+        require(_voteValue == 1, "vote value has to be 1 for all members");
+
+        VotingPoweer[votingPowerCounter] = Voting ({
+          votingId:  _votingId,
+          voteValue: _voteValue,
+          votingPowerGiven: true,
+            voted: false
+
+        });
+    }
 
 
-
-    );
+    function quorumRequirement(uint votesRequired) public onlyOwner {
+         require(Member[msg.sender].memberAdded, "echo Already added");
+           require(VotingPoweer[votingPowerCounter].votingPowerGiven, "echo already voted");
+           require(VotingPoweer[votingPowerCounter].voted, "echo Already voted");
+        require(votesRequired * 100 >= 1 * members, "quarum not met");
+        VotingPoweer[votingPowerCounter].voted = true;
+    }
+ 
 
 
 
 
 
-   }
-
-   function swapTokens(uint amountIn, uint amountOutmin) external onlyOwner {
-    _approve(msg.sender, address(uniswapRouter), amountIn);
-    uniswapRouter.swapExactTokensForTokens(
-        amountIn,
-        amountOutmin,
-        getPath(),
-        address(this),
-          block.timestamp + 3600
-
-
-
-    );
-
-
-   }
-
-   function getPath() public view returns(address[] memory)  {
-    address[] memory path = new address[](2);
-    path[0] = address(this);
-    path[1] = uniswapRouter.WETH();
-    return path;
-
-
-
-
-   }
-
-
-
-
-} 
+}
