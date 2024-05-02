05926f5cc1afa6cd21c846a439151ea0b5d54316
Soham Dutta <123827172+123456788940@users.noreply.github.com>
2024-05-02 10:50:32 +0530

    Update erc20.sol
    Signed-off-by: Soham Dutta <123827172+123456788940@users.noreply.github.com>

--- erc20.sol
+++ erc20.sol
@@ -7,6 +7,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract token is ERC20 {
     constructor() ERC20("MTT", "MultichainZ token"){
-        _mint(msg.sender, 10000000 * 18 ** 10);
+        _mint(msg.sender, 10000000 * 10 ** 18);
     }
 }
