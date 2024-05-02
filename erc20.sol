c0933e5a5bfd3facd3b1b5051aa9f08457d321eb
Soham Dutta <123827172+123456788940@users.noreply.github.com>
2024-05-02 10:24:43 +0530

    Update erc20.sol
    Signed-off-by: Soham Dutta <123827172+123456788940@users.noreply.github.com>

--- erc20.sol
+++ erc20.sol
@@ -1,1 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract token is ERC20 {
+    constructor() ERC20("MTT", "MultichainZ token"){
+        _mint(msg.sender, 10000000 * 10 ** 18);
+    }
+}
